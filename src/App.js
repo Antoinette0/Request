180131081897e99b381db9311066ce3af11e9c4c
Antoinette0 <atolagbeoyinantoinette@gmail.com>
2021-09-20 18:26:06 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return (
+        <div className="App">
+          <div className="Request">
+      <div className="nav">
+      <Navbar className="navbar">
+        <img className="cancel" src="cancel.png" alt="cancel" href="#"/>
+      </Navbar>
+      </div>
+      <div className="check">
+        <img className="tick" src="shape.png" alt="shape" />
+      </div>
+      <div className="info">
+      <h1>Sent!</h1>
+      <p> <img className="add" src="add.png" alt="add"/> olaorosamson@yahoo.com</p>
+      <p className="desc">Will be invited as a member of Zuri Chat once an admin
+         approves the request.</p>
+      </div>
+      <div className="buttons">
+        <button className="btninv">Invite more people</button>
+        <button className="btnd">Finished</button>
+      </div>
+      </div>
     </div>
-  );
+    )
 }
+export default App
 
-export default App;
